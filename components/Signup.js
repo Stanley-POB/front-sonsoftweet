a821f0c064f25f81de6e4a00365b452c779decbc
Mehdi <mfredj6@gmail.com>
2022-10-06 19:51:11 +0200

    update signup

--- components/Signup.js
+++ components/Signup.js
@@ -5,6 +5,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 function Signup() {
+	
+  const [signUpFirstname, setSignUpFirstname] = useState('');
+  const [signUpUsername, setSignUpUsername] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+
+
+
+	const handleRegister = () => {
+		fetch('http://localhost:3000/users/signup', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ firstname: signUpFirstname, username: signUpUsername, password: signUpPassword }),
+		}).then(response => response.json())
+			.then(data => {
+				if (data.result) {
+					dispatch(login({ firstname: signUpFirstname,username: signUpUsername, token: data.token }));
+					setSignUpFirstname('')
+          setSignUpUsername('');
+					setSignUpPassword('');
+					setIsModalVisible(false)
+          window.location.href=''
+				}
+			});
+	};
+
+
   return (
     <>
       <div className={styles.testdiv}>
@@ -13,7 +39,7 @@ function Signup() {
         <input placeholder="First name" className={styles.input}></input>
         <input placeholder="Username" className={styles.input}></input>
         <input placeholder="Password" className={styles.input}></input>
-        <button className={styles.signupbuttonmodal}>Sign Up</button>
+        <button className={styles.signupbuttonmodal} onClick={() => handleRegister()} >Sign Up</button>
       </div>
     </>
   );
